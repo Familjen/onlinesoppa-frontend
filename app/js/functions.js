85b80d4937406aae60a27a9b85169c40389b1704
KimSvenSand <kim.svensson.sand@gmail.com>
2017-03-26 16:40:33 +0200

    Fixed responsive top border, changed some design and fixed angularised input for events

---
+++ app/js/functions.js
@@ -0,0 +1,10 @@
+function createEvent(name, time, place, description) {
+  var mEvent = {
+    'name': name,
+    'time': time,
+    'place': place,
+    'description': description
+  }
+
+  return mEvent;
+}
